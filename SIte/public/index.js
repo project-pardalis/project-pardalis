d3cb15ae8b2b48691f470fdebf9f24cb35c4f2a8
IvanCCO <ivan.miranda@sptech.school>
2022-08-21 12:53:44 -0300

    CSS dash pronto

---
+++ SIte/public/index.js
@@ -0,0 +1,12 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () =>{
+    sideMenu.style.display = 'none';
+})
+
