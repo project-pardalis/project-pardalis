83077dd24daafe04536ea9c6459daa7d9589df67
IvanCCO <ivan.miranda@sptech.school>
2022-08-22 18:21:08 -0300

    Modo escuro

--- SIte/public/index.js
+++ SIte/public/index.js
@@ -1,6 +1,8 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
 
 menuBtn.addEventListener('click', () => {
     sideMenu.style.display = 'block';
@@ -10,3 +12,11 @@ closeBtn.addEventListener('click', () =>{
     sideMenu.style.display = 'none';
 })
 
+// change theme
+
+themeToggler.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-theme-variables')
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+})
