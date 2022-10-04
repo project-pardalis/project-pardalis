3a9d8c2d7d627c9c2383a5321f2e90b8496dfff6
JoaoVictorHengler <joao.hengler@sptech.school>
2022-10-04 15:36:11 -0300

    Removendo a senha de quando é feito o login

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config")
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM Usuario WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
+        SELECT idUsuario, nomeUsuario, emailUsuario, cargo, fkEmpresa, fkAdministrador FROM Usuario WHERE emailUsuario = '${email}' AND senhaUsuario = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
