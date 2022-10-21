da3d8f7d5882afb401cd66f6ca7b8711e09ee2fa
Marco Souza <99811823+Shift39@users.noreply.github.com>
2022-10-21 13:28:47 -0300

    algumas modificações pequenas no looca

---
+++ looca-api-pardalis/src/main/java/com/mycompany/looca/api/pardalis/ConexaoBanco.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.looca.api.pardalis;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ *
+ * @author aluno
+ */
+
+
+public class ConexaoBanco {
+   
+        private JdbcTemplate conexaoBanco;
+        
+        public ConexaoBanco(){
+        
+        BasicDataSource datasource = new BasicDataSource();
+        
+        datasource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        
+        datasource.setUrl("jdbc:mysql://127.0.0.1:3306/teste_de_banco");
+        
+        datasource.setUsername("aluno");
+        
+        datasource.setPassword("sptech");
+        
+        
+        conexaoBanco = new JdbcTemplate(datasource);
+        
+        }
+    
+        
+        
+        
+    public JdbcTemplate getConnection() {
+        return  conexaoBanco;
+    }
+    
+}
