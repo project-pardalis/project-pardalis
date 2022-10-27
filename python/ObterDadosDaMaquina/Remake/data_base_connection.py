63fe3e8bb8dd5f1614bb3fc4f67c7587fd484a83
JoaoVictorHengler <joao.hengler@sptech.school>
2022-10-27 13:38:19 -0300

    Python com melhoria na seleção

--- python/ObterDadosDaMaquina/Remake/data_base_connection.py
+++ python/ObterDadosDaMaquina/Remake/data_base_connection.py
@@ -19,14 +19,42 @@ def run_sql_command(command: str, fetchall: bool = False):
         connection.commit()
     return True
 
+def get_computer_hash(hash: str):
+    command = f"SELECT * FROM Maquina WHERE hashMaquina = '{hash.replace(':', '').upper()}';"
+    return run_sql_command(command, False)
+
 def get_computer_with_hash(hash: str):
-    command = f"SELECT count(*) as qtdServidores FROM Maquina WHERE hashMaquina = '{hash}' LIMIT 1;"
+    command = f"SELECT count(*) as qtdServidores FROM Maquina WHERE hashMaquina = '{hash.replace(':', '').upper()}' LIMIT 1;"
     return run_sql_command(command, False)
 
 def get_componentes_computer(hash: str):
-    command = f"SELECT * FROM Componente JOIN Maquina on idMaquina = fkMaquina where hashMaquina = '{hash}';"
+    command = f"SELECT * FROM Componente JOIN Maquina on idMaquina = fkMaquina where hashMaquina = '{hash.replace(':', '').upper()}';"
     return run_sql_command(command, True)
 
-def get_metricas(id_component: str, fk_maquina: int):
-    command = f"SELECT * FROM Metrica JOIN Componente_has_Metrica on idMetrica = fkMetrica WHERE fkComponente = {id_component} AND fkMaquina = {fk_maquina};"
+def get_metricas():
+    command = f"SELECT * FROM Metrica;"
     return run_sql_command(command, True)
+
+def get_metricas_connection(id_component: str, fk_maquina: int, fk_empresa: int):
+    command = f"SELECT * FROM Metrica JOIN Componente_has_Metrica on idMetrica = fkMetrica WHERE fkComponente = {id_component} AND fkMaquina = {fk_maquina} AND fkEmpresa = {fk_empresa};"
+    return run_sql_command(command, True)
+
+def create_components(fk_maquina: int, fk_empresa: int, components: dict):
+    for key in components:
+        component_state = components[key]
+        command = f"INSERT INTO Componente (nomeComponente, isComponenteValido fkMaquina, fkEmpresa) VALUES ('{key}', {component_state},  {fk_maquina}, {fk_empresa});"
+        run_sql_command(command)
+        create_component_connection_with_metrica(key, fk_maquina, fk_empresa)
+
+def create_component_connection_with_metrica(component_name : str, fk_maquina: int, fk_empresa: int):
+    metricas = get_metricas()
+    for metrica in metricas:
+        if metrica.index(component_name) != -1:
+            command = (f"INSERT INTO Componente_has_Metrica SELECT idComponente, {metrica['idMetrica']}, {fk_maquina}, {fk_empresa}" +
+            f"FROM Componente WHERE nomeComponente = '{component_name}' AND Componente.fkMaquina = {fk_maquina} AND Componente.fkEmpresa = {fk_empresa};")
+            run_sql_command(command)
+
+def append_information(fk_component: int, fk_maquina: int, fk_empresa: int, fk_metrica: int, value: float):
+    data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    command = f"INSERT INTO Leitura VALUES (null, {fk_component}, {fk_metrica}, {fk_maquina}, {fk_empresa}, {data}, {value});"
+    run_sql_command(command)
--- python/ObterDadosDaMaquina/Remake/main.py
+++ python/ObterDadosDaMaquina/Remake/main.py
@@ -7,8 +7,7 @@ import computer_info as ci
 import data_base_connection as db
 
 computer_hash = getmac.get_mac_address().upper()
-print(computer_hash)
-print()
+computer_db = None
 
 def clear_screen():
     if platform.system() == "Linux":
@@ -17,29 +16,30 @@ def clear_screen():
         os.system("cls")
 
 def run_command_verifying_hash(command):
-    if db.get_computer_with_hash(computer_hash.replace(":", "").upper()):
+    if (db.get_computer_with_hash(computer_hash))["count"] == 1:
+        computer_db = db.get_computer_hash(computer_hash)
         command()
+    else:
+        print("Adicione a máquina pelo nosso site!")
 # ----------------------------------------------------------------------------------------------------------------------
 def menu_select():
     while True:
         #clear_screen()
         print("==========================================")
+        print("=              Mac Address:              =")
+        print(f"=           {computer_hash}            =")
         print("=                                        =")
         print("= 1 - Obter informações do Computador    =")
-        print("= 2 - Obter Mac Address desse Computador =")
-        print("= 3 - Obter Estado dos Componentes       =")
-        print("= 4 - Sair                               =")
+        print("= 2 - Obter Estado dos Componentes       =")
+        print("= 3 - Sair                               =")
         print("=                                        =")
         print("==========================================")
         option = input("Escolha uma opção: ")
         if option == "1":
             run_command_verifying_hash(get_computer_info)
-            
         elif option == "2":
-            print(f"O Mac Address desse computador é: {computer_hash}")
-        elif option == "3":
             run_command_verifying_hash(change_components_state)
-        elif option == "4":
+        elif option == "3":
             clear_screen()
             print("Saindo...")
             exit()
@@ -47,25 +47,52 @@ def menu_select():
 # ----------------------------------------------------------------------------------------------------------------------
 #Opção 1
 def get_computer_info():
-    if computer_hash:
         computer_info = ci.get_all_info()
-        print(computer_info)
-        """ computer_components = db.get_componentes_computer(computer_hash)
-        for component in computer_components:
-            metricas_component = db.get_metricas(component["idComponente"], component["fkMaquina"])
-            for metrica in metricas_component:
-                print(f"{metrica['nomeMetrica']}: {computer_info[component['nomeComponente']][metrica['nomeMetrica']]}{metrica['unidadeDeMedida']}") """
+        
+        computer_components = db.get_componentes_computer(computer_hash)
+        if len(computer_components) == 0:
+            ask_for_active_components()
+        else:
+            print("Componentes já cadastrados")
+        
+        get_informations_to_db()
 
-    else:
-        print("Adicione a máquina pelo nosso site!")
+def get_informations_to_db():
+    while True:
+        computer_info = ci.get_all_info()
+        computer_components = db.get_componentes_computer(computer_hash)
+        
+        for component in computer_components:
+            metricas_component = db.get_metricas_connection(component["idComponente"], component["fkMaquina"], component["fkEmpresa"])
+            for metrica in metricas_component:
+                db.append_information(component["idComponente"], component["fkMaquina"], component["fkEmpresa"], metrica["idMetrica"], computer_info[component["nomeComponente"]])
 
 #Opção 2
 def change_components_state():
-    components = db.get_componentes_computer(computer_hash.replace(":", "").upper())
+    components = db.get_componentes_computer("1234567890")#(computer_hash
+
+    print("{:<8} {:<5} {:<30}".format("Nome do Componente", "Estado", "Descrição"))
     for component in components:
-        print(component)
-        print("{:<8} {:<5}".format(component["nomeComponente"], component["isComponenteValido"]))
-        #print(f"{}")
+        print("{:<8} {:<5} {:<30}".format(component["nomeComponente"], component["isComponenteValido"], component["descricao"]))
+
+# ----------------------------------------------------------------------------------------------------------------------
+def ask_for_active_components():
+    components = ["Cpu", "Disco", "Ram"]
+    for component in components:
+        print(f"Gostaria do componente {component} ativo? (S/N)")
+        state_component = False
+        while True:
+            option = input("Resposta: ")
+            if option.upper() == "S":
+                state_component = True
+                break
+            elif option.upper() == "N":
+                state_component = False
+                break
+            else:
+                print("Opção inválida, tente novamente!")
+        db.create_components(computer_db["idMaquina"], computer_db["fkEmpresa"], {component: state_component})
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
