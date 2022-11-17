9f71b33ba07f12f13cf9c5a4f2b4e57ec6d1c804
RafaRpso <rafaelalvesraposo@hotmail.com>
2022-11-17 16:20:42 -0300

    mudança permissao docker

--- script_instalacao/main.sh
+++ script_instalacao/main.sh
@@ -103,9 +103,8 @@ if [ $choose -eq 1 ]; then
   
   sudo docker rm -f pardalis_sql #se tiver, starta pra config
   sudo docker pull mysql/mysql-server:latest 
-  sudo docker run -d -p 3306:3306 --name pardalis_sql -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_USER=root mysql/mysql-server:latest
-  docker exec -it pardalis_sql mysql -uroot -p  
-
+  sudo docker run -d -p 3306:3306 --name pardalis_sql -e MYSQL_ROOT_PASSWORD=urubu100 -e MYSQL_USER=root mysql/mysql-server:latest  
+  sudo docker exec -it pardalis_sql mysql -uroot -p
 
 else 
   echo "Certo! Clone manualmente em  https://github.com/project-pardalis/project-pardalis
